2aecfd892cdcbafc0f47bdcf952095dbd526adca
Precious Ogwezhi <pogwezhi@gmail.com>
2025-06-16 15:02:31 +0100

    initial commit

---
+++ quiz.js
@@ -0,0 +1,13 @@
+function checkAnswer () {
+    const correctAnswer = "4";
+    const feedback = document.getElementById("feedback")
+    let userAnswer = document.querySelector('input[name="quiz"]: checked').value;
+
+    if(userAnswer === correctAnswer){
+        feedback.textContent = "Correct! Well done."
+    }else{
+        feedback.textContent = "That's incorrect! Try again."
+    }
+}
+
+document.getElementById("submit-answer").addEventListener("click", checkAnswer)
